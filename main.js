5da91f2d4d931e656c6df2b72e3115ddd0440277
Taufiqsaefulloh20102237 <20102237@ittelkom-pwt.ac.id>
2023-03-24 11:32:41 +0700

    well done

--- main.js
+++ main.js
@@ -10,3 +10,22 @@ function toggleDetail(e){
     const detil=$(item).children(".about-exp-item-detil")
     $(detil).slideToggle()
 }
+function onFormSubmit(e){
+    e.preventDefault()
+    const email=$("#email")
+    const subject=$("#subject")
+    const message=$("#message")
+
+    if(!$(email).val()){
+        alert ('Email is required')
+    }  else if(!$(subject).val()){
+        alert ('Subject is required')
+    } else if(!$(message).val()){
+        alert ('Message is required')
+    } else{
+        alert ('Form Submited')
+        $(email).val('')
+        $(message).val('')
+        $(message).val('')
+    }
+}
